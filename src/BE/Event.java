56ddec0605cfdd2f025fd80c58879633982d4fb3
JennyTromTheBlock <kenmad01@easv365.dk>
2023-03-21 10:58:35 +0100

    new tableView

--- src/BE/Event.java
+++ src/BE/Event.java
@@ -14,6 +14,62 @@ public class Event {
 
     private Date date;
 
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public List<String> getNotes() {
+        return notes;
+    }
+
+    public void setNotes(List<String> notes) {
+        this.notes = notes;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getMaxParticipant() {
+        return maxParticipant;
+    }
+
+    public void setMaxParticipant(int maxParticipant) {
+        this.maxParticipant = maxParticipant;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
     private int maxParticipant;
     private int price;
     //maybe also available tickets
--- src/GUI/controller/MainViewController.java
+++ src/GUI/controller/MainViewController.java
@@ -1,5 +1,6 @@
 package GUI.controller;
 
+import BE.Event;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
@@ -7,8 +8,10 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
@@ -22,6 +25,7 @@ import javafx.stage.StageStyle;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.ResourceBundle;
 
@@ -44,6 +48,7 @@ public class MainViewController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        //sets all image symbols
         Image searchSymbol = new Image("searchSymbol.png");
         searchButton.setImage(searchSymbol);
 
@@ -56,21 +61,66 @@ public class MainViewController implements Initializable {
         Image cardLogo = new Image("CardList.png");
         cardViewImage.setImage(cardLogo);
 
+
+
+        List<Event> s = new ArrayList<>();
+        ObservableList<Event> list = FXCollections.observableList(s);
+
+        TableView tableView = new TableView();
+
+        //create columns
+        TableColumn<Event, String> column1 =
+                new TableColumn<>("Title");
+        column1.setCellValueFactory(
+                new PropertyValueFactory<>("eventName"));
+
+        TableColumn<Event, String> column2 =
+                new TableColumn<>("location");
+        column2.setCellValueFactory(
+                new PropertyValueFactory<>("location"));
+
+        TableColumn<Event, Integer> column3 =
+                new TableColumn<>("Max participants");
+        column3.setCellValueFactory(
+                new PropertyValueFactory<>("maxParticipant"));
+
+        TableColumn<Event, Integer> column4 =
+                new TableColumn<>("Price");
+        column4.setCellValueFactory(
+                new PropertyValueFactory<>("price"));
+
+        TableColumn<Event, Date> column5 =
+                new TableColumn<>("date");
+        column5.setCellValueFactory(
+                new PropertyValueFactory<>("date"));
+
+
+        //adds all columns to tableView
+        tableView.getColumns().add(column1);
+        tableView.getColumns().add(column2);
+        tableView.getColumns().add(column3);
+        tableView.getColumns().add(column4);
+        tableView.getColumns().add(column5);
+
+
+
         //test
-        List<String> s = new ArrayList<>();
-        s.add("fjeo");
-        s.add("foe");
-        s.add("forgee");
-        s.add("fogregrege");
-        s.add("regregreg");
-        s.add("h65hg4foe");
-        ObservableList obs = FXCollections.observableList(s);
-        ListView<String> listView = new ListView<>();
-        listView.setPrefHeight(800);
-        listView.setItems(obs);
-        contentArea.getChildren().add(listView);
 
+        Date date = new Date();
+        Event event = new Event("fodboldewkopf fest", "kgowrpgeopwfe", "lige her lige nu", date, 50, 20 );
+        Event event1 = new Event("foefmwlpdbold fest", "kgowrpgeopwfe", "lige her lige nu", date, 50, 20 );
+        Event event2 = new Event("håndbold fest", "kgowrpgeopwfe", "lige her lige nu", date, 50, 20 );
+        Event event3 = new Event("fodbold fest", "kgowrpgeopwfe", "lige her lige nu", date, 50, 20 );
+        list.add(event);
+        list.add(event1);
+        list.add(event2);
+        list.add(event3);
+        System.out.println(list.size());
+        System.out.println(tableView.getColumns().size());
+        tableView.setItems(list);
 
+        //adds the tableView to scene
+        contentArea.getChildren().add(tableView);
     }
 
     public void handleCreateEvent(MouseEvent mouseEvent) {
