28c0c0d968c16b7831361537f961c5f9c37cd906
JennyTromTheBlock <kenmad01@easv365.dk>
2023-03-26 20:30:04 +0200

    m

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/GUI/view/MainView.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/GUI/view/PokerMain.fxml"));
         Parent root = loader.load();
         primaryStage.setScene(new Scene(root));
         primaryStage.getScene().getStylesheets().add(getClass().getResource("/GUI/css/Style.css").toExternalForm());
