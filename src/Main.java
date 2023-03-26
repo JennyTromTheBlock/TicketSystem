45804e72cbad9de68ede7c7f49d8b88743eaa17a
JennyTromTheBlock <kenmad01@easv365.dk>
2023-03-26 20:49:52 +0200

    lavet main om

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/GUI/view/PokerMain.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/GUI/view/MainView.fxml"));
         Parent root = loader.load();
         primaryStage.setScene(new Scene(root));
         primaryStage.getScene().getStylesheets().add(getClass().getResource("/GUI/css/Style.css").toExternalForm());
