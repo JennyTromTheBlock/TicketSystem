b8887cc2e41b615767f9355df3e773560d1dcd90
JennyTromTheBlock <kenmad01@easv365.dk>
2023-03-29 23:55:59 +0200

    g

---
+++ src/GUI/controller/adminControllers/AdminBarView.java
@@ -0,0 +1,15 @@
+package GUI.controller.adminControllers;
+
+import javafx.event.ActionEvent;
+
+public class AdminBarView {
+
+    public void handleCreateUser(ActionEvent actionEvent) {
+    }
+
+    public void handleSeeEvents(ActionEvent actionEvent) {
+    }
+
+    public void handleSeeUsers(ActionEvent actionEvent) {
+    }
+}
