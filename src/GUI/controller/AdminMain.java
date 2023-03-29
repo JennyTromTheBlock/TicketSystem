141670513a22ec2fa8015c89acb6e7bb4305de82
JennyTromTheBlock <kenmad01@easv365.dk>
2023-03-29 22:57:14 +0200

    new view

---
+++ src/GUI/controller/AdminMain.java
@@ -0,0 +1,27 @@
+package GUI.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.input.MouseEvent;
+
+public class AdminMain {
+    public void calendarViewBtn(MouseEvent mouseEvent) {
+    }
+
+    public void listViewBtn(MouseEvent mouseEvent) {
+    }
+
+    public void handleCreateUser(ActionEvent actionEvent) {
+    }
+
+    public void handleSeeEvents(ActionEvent actionEvent) {
+    }
+
+    public void handleSeeUsers(ActionEvent actionEvent) {
+    }
+
+    public void handleViewInfo(ActionEvent actionEvent) {
+    }
+
+    public void handleEditEvent(ActionEvent actionEvent) {
+    }
+}
