2faf7c796d92f68faf3e62d4f0b43711e4811581
micdra01 <114472076+micdra01@users.noreply.github.com>
2023-04-10 15:06:51 +0200

    load ticket types

--- src/GUI/controller/specialTicketControllers/SellSpecialTicketController.java
+++ src/GUI/controller/specialTicketControllers/SellSpecialTicketController.java
@@ -6,6 +6,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.List;
@@ -27,13 +28,27 @@ public class SellSpecialTicketController extends BaseController implements Initi
     }
 
     private void loadTicketTypes() {
-        List<SpecialTicketType> allSpecialTickets;
+        List<SpecialTicketType> allSpecialTickets = null;
+        try {
+            allSpecialTickets = getModelsHandler().getSpecialTicketModel().getSpecialTicketTypes();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        for(SpecialTicketType specialTicketType : allSpecialTickets) {
+            MenuItem type = new MenuItem(specialTicketType.getTypeName());
+            type.setOnAction(event -> {
+                lblTicketType.setText(specialTicketType.getTypeName());
+            });
+            mbTicketType.getItems().add(type);
+        }
     }
 
     public void handleConfirm(ActionEvent actionEvent) {
     }
 
     public void handleCancelTicket(ActionEvent actionEvent) {
+        Stage stage = (Stage) txtfCustomerName.getScene().getWindow();
+        stage.close();
     }
 
     public void handleCreateSpecialTicket(ActionEvent actionEvent) {
